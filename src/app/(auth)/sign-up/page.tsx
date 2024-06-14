a96798b69cf24321aab985b7081ae972cf86e9c6
Rahul <thisisrkrkrk@gmail.com>
2024-06-14 22:31:47 +0530

    new

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -101,17 +101,17 @@ export default function SignUpForm() {
 
   return (
     <>
-    
+      <Navbar />
       <div className="flex justify-center items-center min-h-screen bg-black">
         <div className="w-full max-w-screen-lg xl:max-w-screen-xl px-4 md:px-8 py-8 lg:py-12 rounded-lg shadow-2xl bg-black backdrop-blur-md flex flex-col lg:flex-row">
-          <div className="lg:w-3/4 flex justify-center items-center">
+          <div className="lg:w-1/2 flex justify-center items-center">
             <Image
               src={imgurl}
               alt="Main Logo"
               className="w-96 h-96 rounded-full shadow-md mb-2"
             />
           </div>
-          <div className="lg:w-1/2 flex justify-center items-center">
+          <div className="lg:w-3/4 flex justify-center items-center">
             <div className="w-full max-w-md">
               <div className="text-center mb-6">
                 <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-2 text-default">
@@ -133,7 +133,7 @@ export default function SignUpForm() {
                             field.onChange(e);
                             setUsername(e.target.value);
                           }}
-                          className="bg-transparent border-b border-white focus:outline-none focus:border-blue-500 text-white placeholder-white"
+                          className="bg-transparent border-b border-blue-400 focus:outline-none focus:border-blue-500 text-white placeholder-white"
                         />
                         {isCheckingUsername && <Loader2 className="animate-spin" />}
                         {!isCheckingUsername && usernameMessage && (
@@ -160,7 +160,7 @@ export default function SignUpForm() {
                         <Input
                           {...field}
                           name="email"
-                          className="bg-transparent border-b border-white focus:outline-none focus:border-blue-500 text-white placeholder-white"
+                          className="bg-transparent border-b border-blue-400 focus:outline-none focus:border-blue-500 text-white placeholder-white"
                         />
                         <p className="text-gray-200 text-sm">We will send you a verification code</p>
                         <FormMessage />
@@ -178,7 +178,7 @@ export default function SignUpForm() {
                           type="password"
                           {...field}
                           name="password"
-                          className="bg-transparent border-b border-white focus:outline-none focus:border-blue-500 text-white placeholder-white"
+                          className="bg-transparent border-b border-blue-400 focus:outline-none focus:border-blue-500 text-white placeholder-white"
                         />
                         <FormMessage />
                       </FormItem>
@@ -213,104 +213,6 @@ export default function SignUpForm() {
         </div>
       </div>
       <Footer />
-    <Navbar/>
-    <div className="flex justify-center items-center min-h-screen bg-black">
-      <div className="w-full max-w-md p-8 rounded-lg shadow-2xl bg-gray-800 backdrop-blur-md">
-        <div className="text-center">
-          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6 text-default">
-            HoP in YapNap
-          </h1>
-          <p className="mb-4 text-white">Sign up to start your anonymous adventure</p>
-        </div>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-            <FormField
-              name="username"
-              control={form.control}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-white">Username</FormLabel>
-                  <Input
-                    {...field}
-                    onChange={(e) => {
-                      field.onChange(e);
-                      setUsername(e.target.value);
-                    }}
-                    className="bg-transparent border-b border-blue-400 focus:outline-none focus:border-blue-500 text-white placeholder-white"
-                  />
-                  {isCheckingUsername && <Loader2 className="animate-spin" />}
-                  {!isCheckingUsername && usernameMessage && (
-                    <p
-                      className={`text-sm ${
-                        usernameMessage === 'Username is unique'
-                          ? 'text-green-300'
-                          : 'text-red-500'
-                      }`}
-                    >
-                      {usernameMessage}
-                    </p>
-                  )}
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              name="email"
-              control={form.control}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-white">Email</FormLabel>
-                  <Input
-                    {...field}
-                    name="email"
-                    className="bg-transparent border-b border-blue-400 focus:outline-none focus:border-blue-500 text-white placeholder-white"
-                  />
-                  <p className='text-gray-200 text-sm'>We will send you a verification code</p>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-  
-            <FormField
-              name="password"
-              control={form.control}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-white">Password</FormLabel>
-                  <Input
-                    type="password"
-                    {...field}
-                    name="password"
-                    className="bg-transparent border-b border-blue-400 focus:outline-none focus:border-blue-500 text-white placeholder-white"
-                  />
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit" className='w-full bg-default hover:bg-orange-800 text-white' disabled={isSubmitting}>
-              {isSubmitting ? (
-                <>
-                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                  Please wait
-                </>
-              ) : (
-                'Sign Up'
-              )}
-            </Button>
-          </form>
-        </Form>
-        <div className="text-center mt-4">
-          <p className="text-white">
-
-            Already a member?{' '}
-            <Link href="/sign-in" className="text-default hover:text-orange-800">
-              Sign in
-            </Link>
-          </p>
-        </div>
-      </div>
-    </div>
-    <Footer/>
     </>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,7 +21,7 @@ function Navbar() {
           <span className="mr-4 text-default"><h2>Welcome, {user.username || user.email}</h2></span>
           <Link href="/dashboard">
         <Button
-          className="w-full mx-4 md:w-auto bg-white text-black hover:bg-gray-300"
+          className="w-full mx-4 md:w-auto bg-black text-white hover:bg-orange-500"
           variant="outline"
         >
           Dashboard
@@ -37,7 +37,7 @@ function Navbar() {
       </Link>
           <Button
             onClick={() => signOut()}
-            className="w-full md:w-auto bg-white text-black hover:bg-gray-300"
+            className="w-full md:w-auto bg-black text-white hover:bg-default"
             variant="outline"
           >
             Logout
@@ -46,7 +46,7 @@ function Navbar() {
       ) : (
         <Link href="/sign-in">
           <Button
-            className="w-full md:w-auto bg-white text-black hover:bg-gray-300"
+            className="w-full md:w-auto bg-black text-white hover:bg-default"
             variant="outline"
           >
             Login
@@ -55,7 +55,7 @@ function Navbar() {
       )}
       <Link href="/all-users">
         <Button
-          className="w-full mx-4 md:w-auto bg-white text-black hover:bg-gray-300"
+          className="w-full mx-4 md:w-auto bg-black text-white hover:bg-default"
           variant="outline"
         >
           Allusers
